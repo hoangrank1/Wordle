b71a51dcb1a12873ccf1a53e35a0dfb902c5c4d9
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-06-30 00:06:44 +0700

    Show the keyboard

---
+++ client/src/Components/Keyboard/Keyboard.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "./keyboard.css";
+import Key from "../Key/Key";
+import { rootState } from "../interface";
+import {
+  useSelector,
+  useDispatch,
+} from "react-redux";
+import { decPos, setBoard } from "../../redux/boardSlice";
+
+const KeyBoard: React.FC = () => {
+  const rows: string[] = [
+    "q w e r t y u i o p", 
+    "a s d f g h j k l", 
+    "z x c v b n m"
+  ];
+
+  const pos = useSelector((state:rootState) => state.board.pos);
+  const board = useSelector((state:rootState) => state.board.board);
+  const dispatch = useDispatch();
+
+  const clickBack = () => {
+    if (pos <= 0) {
+      return;
+    }
+    const newBoard = [...board];
+    newBoard[pos - 1] = "";
+    dispatch(setBoard(newBoard));
+    dispatch(decPos());
+  }
+
+  return (
+    <div className="keyboard-container">
+      {rows.map((row, idx) => {
+        return (
+          <div key={idx} className="row">
+            {idx === 2 && <span className="letter-row">Enter</span>}
+            {row.split(" ").map((letter, idx) => {
+              return (
+                <div key={idx} className="letter-row">
+                  <Key letter={letter.toUpperCase()} />
+                  {letter === "m" && <span onClick={clickBack}>Back</span>}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default KeyBoard;
