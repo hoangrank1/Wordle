fca81207d8035e825901fa191e3f2e1b25f2db79
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-06-30 00:34:12 +0700

    Animation for the Square

--- client/src/Components/Square/Square.tsx
+++ client/src/Components/Square/Square.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./square.css";
+import {
+  motion
+} from "framer-motion";
 
 interface IProps {
   val: string;
@@ -11,11 +14,30 @@ const Square: React.FC<IProps> = (props) => {
     val,
     squareIdx,
   } = props;
+  const variants = {
+    filled: () => ({
+      scale: [1.2, 1], // scale to 1.2 then comeback to 1
+      transition: {
+        duration: 0.2
+      }
+    }),
+    unfilled: () => ({
+      scale: [1.2, 1], // scale to 1.2 then comeback to 1
+      transition: {
+        duration: 0.2
+      }
+    }),
+  }
 
   return (
-    <div className="square" key={squareIdx}>
-      {val}
-    </div>
+    <motion.div 
+      animate={val ? "filled" : "unfilled"} 
+      variants={variants}
+    >
+      <div className="square" key={squareIdx}>
+        {val}
+      </div>
+    </motion.div>
   );
 };
 
